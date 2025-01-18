06dc09348b72617c7e7a81d88faa7b30baa6fd22
streaam-developer <163761596+streaam-developer@users.noreply.github.com>
2025-01-19 00:55:11 +0530

    Update config.py

--- FileStream/config.py
+++ FileStream/config.py
@@ -7,8 +7,8 @@ load_dotenv("config.env")
 class Telegram:
     API_ID = int(env.get('API_ID', '24010108'))
     API_HASH = str(env.get('API_HASH', '8d89700b2fc09a3aa6c906cbed65b040'))
-    BOT_TOKEN = str(env.get('BOT_TOKEN' , '7185552648:AAEcNkakMyFH9JcsUuF6YeBHLrwdI0JztzY'))
-    OWNER_ID = int(env.get('OWNER_ID', '5791145987'))
+    BOT_TOKEN = str(env.get('BOT_TOKEN' , '7319385528:AAHRuz92jcMD3L7lwKPY-X3HF-az4AK5VyE'))
+    OWNER_ID = int(env.get('OWNER_ID', '6597445442'))
     WORKERS = int(env.get("WORKERS", "6"))  # 6 workers = 6 commands at once
     DATABASE_URL = str(env.get('DATABASE_URL', "mongodb+srv://fehebaw351:nHbjrujWqgqLR58H@cluster0.lekn97z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"))
     UPDATES_CHANNEL = str(env.get('UPDATES_CHANNEL', "Telegram"))
@@ -21,19 +21,19 @@ class Telegram:
     START_PIC = env.get('START_PIC', "https://graph.org/file/290af25276fa34fa8f0aa.jpg")
     VERIFY_PIC = env.get('VERIFY_PIC', "https://graph.org/file/736e21cc0efa4d8c2a0e4.jpg")
     MULTI_CLIENT = False
-    FLOG_CHANNEL = int(env.get("FLOG_CHANNEL", "-1002396030542"))   # Logs channel for file logs
-    ULOG_CHANNEL = int(env.get("ULOG_CHANNEL", "-1002396030542"))   # Logs channel for user logs
+    FLOG_CHANNEL = int(env.get("FLOG_CHANNEL", "-1002290558913"))   # Logs channel for file logs
+    ULOG_CHANNEL = int(env.get("ULOG_CHANNEL", "-1002290558913"))   # Logs channel for user logs
     MODE = env.get("MODE", "primary")
     SECONDARY = True if MODE.lower() == "secondary" else False
     AUTH_USERS = list(set(int(x) for x in str(env.get("AUTH_USERS", "")).split()))
 
 class Server:
-    PORT = int(env.get("PORT", "9062"))
-    BIND_ADDRESS = str(env.get("BIND_ADDRESS", "185.157.213.65"))
+    PORT = int(env.get("PORT", "6761"))
+    BIND_ADDRESS = str(env.get("BIND_ADDRESS", "89.110.81.232"))
     PING_INTERVAL = int(env.get("PING_INTERVAL", "1200"))
     HAS_SSL = str(env.get("HAS_SSL", "0").lower()) in ("1", "true", "t", "yes", "y")
     NO_PORT = str(env.get("NO_PORT", "1").lower()) in ("1", "true", "t", "yes", "y")
-    FQDN = str(env.get("FQDN", "new.streaamstore.shop"))
+    FQDN = str(env.get("FQDN", "free.streaamstore.shop"))
     URL = "http{}://{}{}/".format(
         "s" if HAS_SSL else "", FQDN, "" if NO_PORT else ":" + str(PORT)
     )
